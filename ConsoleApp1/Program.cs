a4f09b632cb90bc5ff6cd0745cf16804ebfa4b5a
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-14 10:49:56 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("please enter the num : ");
+double num = double.Parse(Console.ReadLine());
+if(num > 0)
+{
+    Console.WriteLine("num is positive");
+}
+else
+{
+    Console.WriteLine("num is negative");
+}
+Console.ReadKey();
+
